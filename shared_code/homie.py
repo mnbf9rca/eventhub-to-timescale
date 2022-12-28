d11bb4ce2ce0380f7872869dd57c66d47a9b6ff2
rob al <mnbf9rca@users.noreply.github.com>
2022-12-28 10:34:43 +0000

    document; move all methods to seperate files

---
+++ shared_code/homie.py
@@ -0,0 +1,43 @@
+from typing import Any, List
+
+from azure.functions import EventHubEvent
+
+from .timeseries import create_atomic_record
+from .timeseries import PayloadType
+
+
+def homie_to_timescale(
+    event: EventHubEvent, messagebody: dict, topic: str, publisher: str
+) -> List[dict[str, Any]]:
+    """Convert a homie message to a timescale record
+    @param event: the eventhub event
+    @param messagebody: the message body
+    @param topic: the topic
+    @param publisher: the publisher
+    @return: a list of timescale records
+    """
+    # examine the topic. We're only interested in topics where the last part is one we're interested in
+    events_of_interest = [
+        "measure-temperature",
+        "heating-setpoint",
+        "state",
+        "mode",
+        "thermostat-setpoint",
+    ]
+    lastpart = topic.split("/")[-1]
+    if lastpart not in events_of_interest:
+        return
+    correlation_id = f"{event.enqueued_time.isoformat()}-{event.sequence_number}"
+    # convert the message to a json object
+    return [
+        create_atomic_record(
+            source_timestamp=messagebody["timestamp"],
+            measurement_subject=publisher,
+            measurement_of=lastpart,
+            measurement_value=messagebody["payload"],
+            measurement_data_type=PayloadType.STRING
+            if lastpart in ["state", "mode"]
+            else PayloadType.NUMBER,
+            correlation_id=correlation_id,
+        )
+    ]
