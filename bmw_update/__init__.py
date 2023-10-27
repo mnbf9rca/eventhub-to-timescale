7bc0034768d937097f95cf26c324f540fce9cdb8
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-10-27 20:33:55 +0100

    set return type as List[str] (#92)

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -1,6 +1,7 @@
+from typing import List
 import azure.functions as func
 from shared_code.bmw import get_and_serialise_car_data
 
 
-def main(mytimer: func.TimerRequest) -> None:
+def main(mytimer: func.TimerRequest) -> List[str]:
     return get_and_serialise_car_data()
