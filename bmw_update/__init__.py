ca3b423834b51f983205701de324ca9133c6ccd4
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-10-27 20:18:22 +0000

    list?

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -1,7 +1,8 @@
+from typing import Any
 import azure.functions as func
 from shared_code.bmw import get_and_serialise_car_data
 
 
-def main(mytimer: func.TimerRequest) -> list:
-    data = get_and_serialise_car_data()
+def main(mytimer: func.TimerRequest):
+    data: list = get_and_serialise_car_data()
     return data
