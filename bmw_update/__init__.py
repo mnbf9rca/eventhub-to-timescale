d7424420b0cde54ccd03488654b2936f424a410a
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-10-27 21:11:33 +0100

    list return (#94)

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -2,6 +2,6 @@ import azure.functions as func
 from shared_code.bmw import get_and_serialise_car_data
 
 
-def main(mytimer: func.TimerRequest, outputEventHubMessage: func.Out[list]) -> None:
+def main(mytimer: func.TimerRequest) -> list:
     data = get_and_serialise_car_data()
-    outputEventHubMessage.set(data)
+    return data
