6540e6c338136985ac05cfbaa7b32943aa8f3489
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-10-27 16:57:13 +0000

    create the actual azure function

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -1,14 +1,6 @@
-import datetime
-import logging
-
 import azure.functions as func
+from shared_code.bmw import get_and_serialise_car_data
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    return get_and_serialise_car_data()
