bc244f0b2cfc58c526feaa6539c0902b2ff3b017
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-10-27 19:45:36 +0000

    use funct.Out

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -1,7 +1,7 @@
-from typing import List
 import azure.functions as func
 from shared_code.bmw import get_and_serialise_car_data
 
 
-def main(mytimer: func.TimerRequest) -> List[str]:
-    return get_and_serialise_car_data()
+def main(mytimer: func.TimerRequest, outputEventHubMessage: func.Out[list]) -> None:
+    data = get_and_serialise_car_data()
+    outputEventHubMessage.set(data)
