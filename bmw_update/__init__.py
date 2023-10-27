b68b4657b14ba6a249040aa032c181936c56fdf7
Sourcery AI <>
2023-10-27 20:11:02 +0000

    'Refactored by Sourcery'

--- bmw_update/__init__.py
+++ bmw_update/__init__.py
@@ -3,5 +3,4 @@ from shared_code.bmw import get_and_serialise_car_data
 
 
 def main(mytimer: func.TimerRequest) -> list:
-    data = get_and_serialise_car_data()
-    return data
+    return get_and_serialise_car_data()
