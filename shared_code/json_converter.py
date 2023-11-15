d92ed3f26507fd692642e5d8f85877ac562e7954
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-11-15 11:50:58 +0000

    Add support for multiple events in json_converter.py

--- shared_code/json_converter.py
+++ shared_code/json_converter.py
@@ -1,6 +1,7 @@
 import json
 import logging
-from typing import Any, List
+import itertools
+from typing import Any, List, Iterator
 from shared_code.glow import glow_to_timescale
 from shared_code.homie import homie_to_timescale
 from shared_code.emon import emon_to_timescale
@@ -11,42 +12,101 @@ import azure.functions as func
 
 
 def convert_json_to_timeseries(
-    event: func.EventHubEvent, outputEventHubMessage: func.Out[List[str]]
+    events: List[func.EventHubEvent] | List[str],
+    outputEventHubMessage: func.Out[List[str]],
 ) -> None:
-    return_value = []
-
-    events_to_process = event if isinstance(event, list) else [event]
-    for event in events_to_process:
-        result = parse_message(event)
-        if result is not None:
-            return_value.append(result)
-    if len(return_value) > 0:
-        for p in return_value:
-            outputEventHubMessage.set(json.dumps(p))
+    array_of_events_as_strings = [get_event_as_str(event) for event in to_list(events)]
+    messages: filter[list[dict[str, Any]]] = filter(
+        None, (convert_event(event) for event in array_of_events_as_strings)
+    )
+    send_messages(messages, outputEventHubMessage)
 
 
-def parse_message(event: str):
+def to_list(events):
+    return events if isinstance(events, list) else [events]
+
+
+def get_event_as_str(event):
     try:
-        if isinstance(event, str):
-            o_messagebody = json.loads(event)
-        elif isinstance(event, func.EventHubEvent):
-            o_messagebody = json.loads(event.get_body().decode("utf-8"))
-        topic, publisher = extract_topic(o_messagebody)
+        message_json = json.dumps(event)
+    except json.JSONDecodeError:
+        message_json = "<non-serializable object>"
     except Exception as e:
-        logging.error(f"Error parsing message: {e}")
-        raise
+        message_json = "<unknown object>"
+        logging.error(f"Error serializing event in get_event_as_str: {e}")
 
-    try:
-        payload: List[dict[str, Any]] = send_to_converter(
-            publisher, o_messagebody, topic
+    if not isinstance(event, str) and not isinstance(event, func.EventHubEvent):
+        error_message = (
+            f"Event {message_json} is of type: {type(event)} not str or EventHubEvent"
         )
-    except Exception as e:
-        logging.error(f"Error converting message: {e}")
-        raise
+        logging.debug(error_message)
+        raise TypeError(error_message)
+    return event if isinstance(event, str) else event.get_body().decode("utf-8")
 
-    logging.debug(f"Parsed payload: {payload}")
-    return payload if payload else None
-    # return [json.dumps(p) for p in payload] if payload else None
+
+def convert_event(event_str):
+    try:
+        o_messagebody = json.loads(event_str)
+        topic, publisher = extract_topic(o_messagebody)
+        payload = send_to_converter(publisher, o_messagebody, topic)
+        logging.debug(f"Parsed payload: {payload}")
+        return payload if payload else None
+    except Exception as e:
+        logging.error(f"Error in event conversion: {e}")
+        return None
+
+
+def send_messages(
+    messages: Iterator[Any], outputEventHubMessage: func.Out[List[str]]
+) -> None:
+    for message in messages:
+        try:
+            payload = json.dumps(message)
+            outputEventHubMessage.set(payload)
+        except (json.JSONDecodeError, ValueError, TypeError):
+            logging.error(f"json_converter: Error serializing message: {message}")
+        except Exception as e:
+            logging.error(f"json_converter: Error sending message: {e}")
+
+
+# def convert_json_to_timeseries(
+#     event: List[func.EventHubEvent] | List[str],
+#     outputEventHubMessage: func.Out[List[str]],
+# ) -> None:
+#     return_value = []
+
+#     events_to_process = event if isinstance(event, list) else [event]
+#     for event in events_to_process:
+#         result = parse_message(event)
+#         if result is not None:
+#             return_value.append(result)
+#     if len(return_value) > 0:
+#         for p in return_value:
+#             outputEventHubMessage.set(json.dumps(p))
+
+
+# def parse_message(event: str):
+#     try:
+#         if isinstance(event, str):
+#             o_messagebody = json.loads(event)
+#         elif isinstance(event, func.EventHubEvent):
+#             o_messagebody = json.loads(event.get_body().decode("utf-8"))
+#         topic, publisher = extract_topic(o_messagebody)
+#     except Exception as e:
+#         logging.error(f"Error parsing message: {e}")
+#         raise
+
+#     try:
+#         payload: List[dict[str, Any]] = send_to_converter(
+#             publisher, o_messagebody, topic
+#         )
+#     except Exception as e:
+#         logging.error(f"Error converting message: {e}")
+#         raise
+
+#     logging.debug(f"Parsed payload: {payload}")
+#     return payload if payload else None
+# return [json.dumps(p) for p in payload] if payload else None
 
 
 def send_to_converter(
--- shared_code/test/test_json.py
+++ shared_code/test/test_json.py
@@ -1,24 +1,28 @@
 import pytest
+import json
+import datetime
+from unittest.mock import Mock, patch
+import azure.functions as func
 
-from shared_code import json
+from shared_code import json_converter
 
 
 class TestSendToConverter:
     @pytest.fixture()
     def mock_glow_to_timescale(self, mocker):
-        return mocker.patch("shared_code.json.glow_to_timescale")
+        return mocker.patch("shared_code.json_converter.glow_to_timescale")
 
     @pytest.fixture()
     def mock_homie_to_timescale(self, mocker):
-        return mocker.patch("shared_code.json.homie_to_timescale")
+        return mocker.patch("shared_code.json_converter.homie_to_timescale")
 
     @pytest.fixture()
     def mock_emon_to_timescale(self, mocker):
-        return mocker.patch("shared_code.json.emon_to_timescale")
+        return mocker.patch("shared_code.json_converter.emon_to_timescale")
 
     @pytest.fixture()
     def mock_logger(self, mocker):
-        return mocker.patch("shared_code.json.logging")
+        return mocker.patch("shared_code.json_converter.logging")
 
     @pytest.mark.parametrize(
         "publisher, expected_converter_name",
@@ -47,7 +51,9 @@ class TestSendToConverter:
         mock_messagebody = "mock_messagebody"
         mock_topic = "mock_topic"
 
-        result = json.send_to_converter(publisher, mock_messagebody, mock_topic)
+        result = json_converter.send_to_converter(
+            publisher, mock_messagebody, mock_topic
+        )
         for converter in all_converters:
             if converter == expected_converter:
                 converter.assert_called_once_with(
@@ -74,8 +80,139 @@ class TestSendToConverter:
         mock_topic = "mock_topic"
 
         with pytest.raises(ValueError) as e:
-            json.send_to_converter(mock_publisher, mock_messagebody, mock_topic)
+            json_converter.send_to_converter(
+                mock_publisher, mock_messagebody, mock_topic
+            )
         assert str(e.value) == f"Unknown publisher: {mock_publisher}"
         for converter in all_converters:
             converter.assert_not_called()
         assert mock_logger.error.call_count == 1
+
+
+class TestGetEventAsStr:
+    @staticmethod
+    def create_eventhub_event(body: str):
+        return func.EventHubEvent(
+            body=body.encode("UTF-8"),
+            enqueued_time=datetime.datetime.now(),
+            offset=0,
+            sequence_number=0,
+        )
+
+    @pytest.mark.parametrize(
+        "input_event, expected",
+        [
+            ("test string", "test string"),
+            (create_eventhub_event("test event body"), "test event body"),
+        ],
+    )
+    def test_get_event_as_str_success(self, input_event, expected):
+        assert json_converter.get_event_as_str(input_event) == expected
+
+    @pytest.mark.parametrize(
+        "input_event, exception, expected_message",
+        [
+            (
+                Mock(),
+                json.JSONDecodeError("Could not serialize", "", 0),
+                "<non-serializable object>",
+            ),
+            (Mock(), Exception("General exception"), "<unknown object>"),
+        ],
+    )
+    def test_get_event_as_str_exceptions(
+        self, mocker, input_event, exception, expected_message
+    ):
+        mocker.patch("json.dumps", side_effect=exception)
+        with pytest.raises(TypeError) as exc_info:
+            json_converter.get_event_as_str(input_event)
+        assert expected_message in str(exc_info.value)
+
+    @pytest.mark.parametrize(
+        "input_event, expected_exception_message",
+        [
+            (12345, "Event 12345 is of type: <class 'int'> not str or EventHubEvent"),
+            (
+                [1, 2, 3],
+                "Event [1, 2, 3] is of type: <class 'list'> not str or EventHubEvent",
+            ),
+            (
+                {"key": "value"},
+                'Event {"key": "value"} is of type: <class \'dict\'> not str or EventHubEvent',
+            ),
+        ],
+    )
+    def test_get_event_as_str_type_error(self, input_event, expected_exception_message):
+        with pytest.raises(TypeError) as exc_info:
+            json_converter.get_event_as_str(input_event)
+        assert str(exc_info.value) == expected_exception_message
+
+
+class TestSendMessages:
+    @pytest.fixture
+    def mock_output_event_hub_message(self):
+        mock = Mock(spec=func.Out)
+        return mock
+
+    @pytest.mark.parametrize(
+        "messages, expected_message_count",
+        [
+            (["message1", "message2", "message3"], 3),
+            (iter(["message1", "message2", "message3"]), 3),  # testing with an iterator
+        ],
+    )
+    def test_send_messages_success(
+        self, messages, expected_message_count, mock_output_event_hub_message
+    ):
+        json_converter.send_messages(messages, mock_output_event_hub_message)
+        assert mock_output_event_hub_message.set.call_count == expected_message_count
+
+    @pytest.mark.parametrize(
+        "message, exception",
+        [
+            ("non-serializable object", TypeError()),
+            ("another non-serializable object", ValueError()),
+        ],
+    )
+    def test_send_messages_json_dump_failure(
+        self, message, exception, mock_output_event_hub_message
+    ):
+        with patch("json.dumps", side_effect=exception):
+            with patch("logging.error") as mock_logging_error:
+                json_converter.send_messages([message], mock_output_event_hub_message)
+                mock_logging_error.assert_called_once()
+                mock_output_event_hub_message.set.assert_not_called()
+
+    def test_send_messages_unexpected_exception(self, mock_output_event_hub_message):
+        with patch("json.dumps", side_effect=Exception("Unexpected error")):
+            with patch("logging.error") as mock_logging_error:
+                json_converter.send_messages(["message"], mock_output_event_hub_message)
+                mock_logging_error.assert_called_once()
+                mock_output_event_hub_message.set.assert_not_called()
+
+    @patch("shared_code.json_converter.logging.error")
+    @patch("shared_code.json_converter.json")
+    def test_partial_failure_in_serialization(
+        self, mock_json, mock_logging_error, mock_output_event_hub_message
+    ):
+        messages = ["message1", "message2", "message3"]
+
+        # Define custom behavior for json.dumps
+        def side_effect_for_json_dumps(message):
+            if message == "message2":
+                raise ValueError("Serialization Error")
+            return json.dumps(message)
+
+        # mock_json_dumps.side_effect = side_effect_for_json_dumps
+        mock_json.JSONDecodeError = json.JSONDecodeError
+        mock_json.dumps.side_effect = side_effect_for_json_dumps
+
+        json_converter.send_messages(messages, mock_output_event_hub_message)
+
+        # Check that set is called twice (for message1 and message3)
+        assert mock_output_event_hub_message.set.call_count == 2
+
+        # Check that the correct log message is recorded
+        mock_logging_error.assert_called_once_with(
+            "json_converter: Error serializing message: message2"
+        )
